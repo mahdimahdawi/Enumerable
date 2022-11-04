61ccb6e1295d54d929b861ffa76a8f20069123ec
melashu <meshu102@outlook.com>
2022-11-04 14:50:16 +0300

    Add tests

--- my_list.rb
+++ my_list.rb
@@ -10,9 +10,17 @@ class  MyList
  end
   
 end
+
 number_list = [1,2,3,4]
 list = MyList.new(number_list)
+# Test #all?
 puts list.all? {|e| e < 5}
 puts list.all? {|e| e > 5}
+
+#Test #any 
 puts list.any? {|e| e == 2}
 puts list.any? {|e| e == 5}
+
+#Test filter 
+print list.filter {|e| e.odd?}
+print list.filter {|e| e.even?}
