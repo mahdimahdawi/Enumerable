716b2773be19840767d0a35eff13e4739b2600ba
melashu <meshu102@outlook.com>
2022-11-04 15:00:27 +0300

    Add fix to linter error

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,23 +1,23 @@
 module MyEnumerable
   def all?
     each do |n|
-    return false unless yield n
+      return false unless yield n
     end
     true
   end
+
   def any?
     each do |n|
-      return true if yield n      
+      return true if yield n
     end
     false
   end
 
-  def filter 
-    lists=[]
+  def filter
+    lists = []
     each do |n|
-      lists << n if yield n     
+      lists << n if yield n
     end
     lists
   end
-
 end
--- my_list.rb
+++ my_list.rb
@@ -1,26 +1,25 @@
-require_relative "my_enumerable"
-include MyEnumerable
-
-class  MyList
+require_relative 'my_enumerable'
+class MyList
+  include MyEnumerable
   def initialize(list)
     @list = list
   end
- def each(&lists)
-  @list.each(&lists)
- end
-  
+
+  def each(&lists)
+    @list.each(&lists)
+  end
 end
 
-number_list = [1,2,3,4]
+number_list = [1, 2, 3, 4]
 list = MyList.new(number_list)
 # Test #all?
-puts list.all? {|e| e < 5}
-puts list.all? {|e| e > 5}
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
 
-#Test #any 
-puts list.any? {|e| e == 2}
-puts list.any? {|e| e == 5}
+# Test #any
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
 
-#Test filter 
-print list.filter {|e| e.odd?}
-print list.filter {|e| e.even?}
+# Test filter
+print list.filter(&:odd?)
+print list.filter(&:even?)
